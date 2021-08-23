43214d234826b6d90c9cf037c3cc6e793d32b125
Raphael Leal <raphaelleal990@gmail.com>
2021-08-23 02:15:05 +0000

    Atualização

---
+++ EXER3.java
@@ -0,0 +1,10 @@
+public class EXER2{
+    public static void main(String[]args){
+        for(int x = 0; x <= 100; x++){
+            if(x % 3 == 0){
+                Sytem.out.println(x);
+            }
+        }   
+        
+    }
+}
